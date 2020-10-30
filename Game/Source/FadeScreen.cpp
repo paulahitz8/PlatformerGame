cbbbc0c5efd80cab320a2183e41076cea4146b95
mav006 <59050192+mav006@users.noreply.github.com>
2020-10-30 10:42:38 +0100

    Fadescreen try#2

--- Game/Source/FadeScreen.cpp
+++ Game/Source/FadeScreen.cpp
@@ -10,7 +10,7 @@
 //
 //FadeScreen::FadeScreen() : Module()
 //{
-//	screenRect = { 0, 0, app->win->GetWidth(), app->win->GetHeight() };
+//	screenRect = { 0, 0, (int) app->win->GetWidth(), (int) app->win->GetHeight() };
 //}
 //
 //// Destructor
@@ -44,7 +44,7 @@
 //bool FadeScreen::Update(float dt)
 //{
 //	// Exit this function if we are not performing a fade
-//	if (currentStep == Fade_Step::NONE) return Update_Status::UPDATE_CONTINUE;
+//	if (currentStep == Fade_Step::NONE) return true;
 //
 //	if (currentStep == Fade_Step::TO_BLACK)
 //	{
@@ -72,12 +72,40 @@
 //// Called each loop iteration
 //bool FadeScreen::PostUpdate()
 //{
-//	return false;
-//}
 //
-//// Called before quitting
-//bool FadeScreen::CleanUp()
-//{
+//	// Exit this function if we are not performing a fade
+//	if (currentStep == Fade_Step::NONE) return true;
+//
+//	float fadeRatio = (float)frameCount / (float)maxFadeFrames;
+//
+//	// Render the black square with alpha on the screen
+//	SDL_SetRenderDrawColor(app->render->renderer, 0, 0, 0, (Uint8)(fadeRatio * 255.0f));
+//	SDL_RenderFillRect(app->render->renderer, &screenRect);
+//
 //	return true;
 //}
 //
+//bool FadeScreen::FadeToBlack(Module* moduleToDisable, Module* moduleToEnable, float frames)
+//{
+//
+//	// If we are already in a fade process, ignore this call
+//	if (currentStep == Fade_Step::NONE)
+//	{
+//		currentStep = Fade_Step::TO_BLACK;
+//		frameCount = 0;
+//		maxFadeFrames = frames;
+//
+//		this->moduleToDisable = moduleToDisable;
+//		this->moduleToEnable = moduleToEnable;
+//
+//		return true;
+//	}
+//	else
+//	{
+//		currentStep = Fade_Step::FROM_BLACK;
+//		frameCount = frames;
+//		maxFadeFrames = frames;
+//
+//		return true;
+//	}
+//}
