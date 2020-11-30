81dc26efe69123ec48b93b617e8b51652e184802
ihedud <irene.hepo@gmail.com>
2020-11-30 11:23:12 +0100

    ajn

--- Game/Source/App.cpp
+++ Game/Source/App.cpp
@@ -42,7 +42,7 @@ App::App(int argc, char* args[]) : argc(argc), args(args)
 	deathScreen = new DeathScreen();
 	winScreen = new WinScreen();
 	enemies = new Enemies();
-	path = new PathFinding();
+	//path = new PathFinding();
 
 	// Ordered for awake / Start / Update
 	// Reverse order of CleanUp
