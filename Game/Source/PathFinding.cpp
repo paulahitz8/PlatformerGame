a7e70fc44039327c70b941c6160d5eeda3fde074
mav006 <59050192+mav006@users.noreply.github.com>
2020-12-01 17:16:39 +0100

    PATHFINDING PORFIN

--- Game/Source/PathFinding.cpp
+++ Game/Source/PathFinding.cpp
@@ -1,113 +1,177 @@
-//#include "App.h"
-//#include "PathFinding.h"
-//#include "Player.h"
-//#include "List.h"
-//#include <unordered_map>
-//#include <queue> 
-//
-//#include "Defs.h"
-//#include "Log.h"
-//
-//
-//template<typename T, typename priority_t>
-//struct PriorityQueue {
-//	typedef std::pair<priority_t, T> PQElement;
-//	std::priority_queue<PQElement, std::vector<PQElement>,
-//		std::greater<PQElement>> elements;
-//
-//	inline bool empty() const {
-//		return elements.empty();
-//	}
-//
-//	inline void put(T item, priority_t priority) {
-//		elements.emplace(priority, item);
-//	}
-//
-//	T get() {
-//		T best_item = elements.top().second;
-//		elements.pop();
-//		return best_item;
-//	}
-//};
-//
-//
-//PathFinding::PathFinding() : Module()
-//{
-//	name.Create("pathfinding");
-//
-//}
-//
-//// Destructor
-//PathFinding::~PathFinding()
-//{
-//	//RELEASE_ARRAY(map);
-//}
-//
-//int PathFinding::Heuristic(iPoint start, iPoint end)
-//{
-//	return abs(start.x - end.x) + abs(start.y - end.y);
-//}
-//
-//List<iPoint> Neighbours(iPoint current, int speed)
-//{
-//	List<iPoint> list = List<iPoint>();
-//	// Order is top, right, bottom, left
-//	list.Add(iPoint(current.x, current.y - speed));
-//	list.Add(iPoint(current.x + speed, current.y));
-//	list.Add(iPoint(current.x, current.y - speed));
-//	list.Add(iPoint(current.x - speed, current.y));
-//
-//	return list;
-//
-//}
-//
-//iPoint PathFinding::NextMove(std::unordered_map<iPoint, iPoint> cameFrom, iPoint end, iPoint start) 
-//{
-//	iPoint current = end;
-//
-//	while (cameFrom[current] != start)
-//	{
-//		current = cameFrom[current];
-//	}
-//	return current; 
-//}
-//
-//iPoint PathFinding::Path(iPoint start, int speed) 
-//{
-//	
-//	PriorityQueue<iPoint, double> frontier;
-//	std::unordered_map<iPoint, iPoint> came_from; 
-//	std::unordered_map<iPoint, int> cost_so_far;
-//	frontier.put(start, 0);
-//	iPoint end = app->player->playerPos;
-//	came_from[start] = start;
-//	cost_so_far[start] = 0;
-//
-//	while (!frontier.empty()) 
-//	{
-//		iPoint current = frontier.get();
-//
-//		if (current == end)
-//		{
-//			break;
-//		}
-//		List<iPoint> neighbours = Neighbours(current, speed);
-//		for (int i = 0; i < 4; i++) 
-//		{
-//			double new_cost = cost_so_far[current] + speed;
-//			iPoint next = neighbours.At(i)->data;
-//			if (cost_so_far.find(next) == cost_so_far.end() || new_cost < cost_so_far[next]) 
-//			{
-//				cost_so_far[next] = new_cost;
-//				double priority = new_cost + Heuristic(next, end);
-//				frontier.put(next, priority);
-//				came_from[next] = current;
-//			}
-//		}
-//	}
-//
-//	return NextMove(came_from, end, start);
-//
-//}
+#include "App.h"
+#include "PathFinding.h"
+#include "Player.h"
+#include "List.h"
+#include "NodeAstar.h"
+#include <unordered_map>
+#include <queue> 
+
+#include "Defs.h"
+#include "Log.h"
+
+using namespace std; 
+
+PathFinding::PathFinding() : Module()
+{
+	name.Create("pathfinding");
+
+}
+
+// Destructor
+PathFinding::~PathFinding()
+{
+	//RELEASE_ARRAY(map);
+}
+
+int PathFinding::Heuristic(iPoint start, iPoint end)
+{
+	return abs(start.x - end.x) + abs(start.y - end.y);
+}
+
+List<iPoint> Neighbours(iPoint current, int speed)
+{
+	List<iPoint> list = List<iPoint>();
+	// Order is top, right, bottom, left
+	list.Add(iPoint(current.x, current.y - speed));
+	list.Add(iPoint(current.x + speed, current.y));
+	list.Add(iPoint(current.x, current.y - speed));
+	list.Add(iPoint(current.x - speed, current.y));
+
+	return list;
+
+}
+
+iPoint PathFinding::NextMove(NodeAstar* endNode, iPoint start)
+{
+	NodeAstar* current = endNode;
+
+	while (current->GetPreviousNode()->GetPos() != start)
+	{
+		current = current->GetPreviousNode(); 
+	}
+	return current->GetPos();
+}
+
+bool PathFinding::isVisited(iPoint nodePoint, List<NodeAstar*> visited) 
+{
+	ListItem<NodeAstar*>* current = visited.At(0);
+	while (current != NULL) 
+	{
+		if (current->data->GetPos() == nodePoint) {
+			return true;
+		}
+
+		current = current->next;
+	}
+
+	return false;
+}
+
+priority_queue<NodeAstar*> PathFinding::UpdateNeighbours(priority_queue<NodeAstar*> nodes, 
+	iPoint neighbourPoint, int newCost, NodeAstar* curr, NodeAstar* endNode, List<NodeAstar*> visited)
+{	int manhattan = Heuristic(neighbourPoint, endNode->GetPos());
+
+	// Create a variable for saving the lost nodes for finding the desired node
+	priority_queue<NodeAstar*> save;
+
+	// Taking first value
+	NodeAstar* current = nodes.top();
+	nodes.pop(); 
+	NodeAstar* neighbourNode; 
+
+	//Create a bool for stoping the iteration
+	bool foundNode = false; 
 
 
+	while (current != NULL || foundNode != true)
+	{
+		// Node in queue
+		if (current->GetPos() == neighbourPoint) {
+			// If the new calculated cost is lower, change the whole node and push it into the auxiliar queue
+			if (current->GetCost() > newCost)
+			{
+				current->SetCost(newCost);
+				current->SetPreviousNode(curr); 
+				current->SetPrio(newCost + manhattan);
+				save.push(current); 
+
+			}
+			else 
+			{
+				// If the new calculated node is worse, push it to the auxiliar queue without modifying it
+				save.push(current); 
+			}
+			foundNode = true; 
+		}
+		// Node not in queue
+		else 
+		{
+			// Push the node in the auxiliar queue and find next element in main queue
+			save.push(current);
+			current = nodes.top();
+			nodes.pop(); 
+		}
+
+		// Check if the neighbour isn't on the visited list or on the priority queue
+		if (isVisited(neighbourPoint, visited) == false || foundNode==false)
+		{
+			// Create the new node from neighbour
+			neighbourNode = new NodeAstar(curr, newCost, neighbourPoint);
+
+			// Calculate it's priority and push it into the priority queue
+			int priority = newCost + manhattan;
+			neighbourNode->SetPrio(priority); 
+			nodes.push(neighbourNode);
+		}
+	
+	return nodes; 
+		
+	}
+
+	// Iterate over the auxiliar queue and add all it's items on the main queue again. 
+	while (save.empty() == false) {
+		nodes.push(save.top()); 
+		save.pop(); 
+	}
+
+}
+
+iPoint PathFinding::Path(iPoint start, int speed)
+{
+	NodeAstar* startNode = new NodeAstar(nullptr, 0, start);
+	int manhattan; 
+	std::priority_queue<NodeAstar*> frontier;
+	frontier.push(startNode); 
+
+	iPoint end = app->player->playerPos;
+	NodeAstar* endNode = new NodeAstar(nullptr, 0, start);
+
+	List<NodeAstar*> visited;
+	
+	NodeAstar* current = frontier.top(); 
+
+	while (current!=endNode)
+	{
+		NodeAstar* current = frontier.top();
+		frontier.pop(); 
+		visited.Add(current);
+
+		if (current->GetPos() == end)
+		{
+			break;
+		}
+
+		List<iPoint> neighbours = Neighbours(current->GetPos(), speed);
+		for (int i = 0; i < 4; i++)
+		{
+			int newCost = current->GetCost() + speed;
+			iPoint next = neighbours.At(i)->data;
+			frontier = UpdateNeighbours(frontier, neighbours[i], newCost, current, endNode, visited);
+		}
+	}
+
+	return NextMove(current, start);
+
+}
+
+//CLEANUUUUUUUP
