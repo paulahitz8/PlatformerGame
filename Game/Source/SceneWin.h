ba5f9c55340cc628ca10ee71862ff4b7a72e67bd
mav006 <59050192+mav006@users.noreply.github.com>
2021-01-05 19:51:17 +0100

    winscreen and lose screen buttons

--- Game/Source/SceneWin.h
+++ Game/Source/SceneWin.h
@@ -4,6 +4,8 @@
 #include "Scene.h"
 #include "Player.h"
 
+#include "GuiButton.h"
+
 struct SDL_Texture;
 
 class SceneWin : public Scene
@@ -21,7 +23,32 @@ public:
 
 	bool Unload();
 
+	// Declare on mouse click event
+	bool OnGuiMouseClickEvent(GuiControl* control);
+
+	bool exitReq = true;
+	bool creditsTab = false;
+	bool settingsTab = false;
+
 private:
+
+	//buttons
+	GuiButton* btnCredits;
+	GuiButton* btnPlay;
+	GuiButton* btnExit;
+	GuiButton* btnSettings;
+
+	GuiButton* btnCredCross;
+	GuiButton* btnSettCross;
+
+	SDL_Texture* credits;
+	SDL_Texture* settings;
+
+	SDL_Rect rectTitle;
+	SDL_Rect rectCredit;
+	SDL_Rect rectSettings;
+
+	//scene
 	SDL_Texture* sceneWon0;
 	SDL_Texture* sceneWon12;
 	SDL_Texture* sceneWon34;
